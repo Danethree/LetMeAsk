46736ee0181a3ac5ca74e294705ae9481808f841
fialho <danidaninpce3@gmail.com>
2021-06-22 01:52:11 -0300

    Creating pages and initializing google authentication

---
+++ src/pages/Home.tsx
@@ -0,0 +1,51 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/Button';
+import {useHistory} from 'react-router-dom'
+import {auth,firebase} from '../services/firebase';
+export function Home(){
+
+  const history = useHistory(); //os hooks precisam estar dentro do componente
+
+  function handleCreateRoom() 
+  {     
+    const provider = new firebase.auth.GoogleAuthProvider();
+            auth.signInWithPopup(provider).then(result => {
+             history.push('/rooms/new');
+    })
+  }
+  
+  return(
+    <div id = "page-auth">
+      <aside>
+              <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
+              <strong>Crie salas de Q&amp;A ao vivo</strong>
+              <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+            <main>
+                    <div className = "main-content">
+                          <img src={logoImg} alt="Let Me Ask"/>
+                          <button onClick = {handleCreateRoom} className = "create-room">
+                                <img src={googleIconImg} alt = "Logo do Google"/>
+                                Crie sua sala com o Google
+                          </button>
+                          <div className = "separator"> Ou entre em uma sala </div>
+                          <form>
+                            <input 
+                                    type="text"
+                                    placeholder="Digite o código da sala"
+                            />
+                           
+
+                          </form>
+
+                          <Button type = "submit">
+                                Entrar na sala
+                            </Button>
+                    </div>
+            </main>
+    </div>
+  )
+}
